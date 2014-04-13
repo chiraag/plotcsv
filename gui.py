07e8173dec345b2b26d1b8c7d3efda3a3c9f3a4a
mtikekar <mtikekar-Satellite-U845>
2014-04-13 13:56:18 -0400

    Minor fixes to gui. Added profiling

--- gui.py
+++ gui.py
@@ -2,8 +2,8 @@ from __future__ import print_function
 import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
-
 import plot_settings
+from profilehooks import profile
 
 class TagAxisItem(pg.AxisItem):
     def setTickStrings(self, strings):
@@ -13,10 +13,13 @@ class TagAxisItem(pg.AxisItem):
         return [self._tickStr.get(va*scale, '') for va in values]
 
 class TracePlotItem(pg.ScatterPlotItem):
-    def __init__(self, df, settings, textBox):
+    @profile
+    def __init__(self, parent, df, settings):
+        print("start trace init")
         pg.ScatterPlotItem.__init__(self, size=1, symbol='s', pen=None, pxMode=False)
-        self.textBox = textBox
+
         self.settings = settings
+        self.textBox = pg.TextItem(color="#ffffff", fill=(20,20,20), anchor=(1,1), border="#ffffff")
 
         x = settings.get_cycle_count(np.array(df.index)) + 0.5
         y = np.array(df.tag.apply(settings.get_row_pos))
@@ -25,29 +28,32 @@ class TracePlotItem(pg.ScatterPlotItem):
         self.addPoints(x=x, y=y, brush=c)
         self.idx_by_tag = df.set_index('tag', append=True)
 
-    def mouseClickEvent(self, ev):
-        time = self.settings.get_time(ev.pos().x())
-        tag = self.settings.get_tag(ev.pos().y())
-        try:
-            val = self.idx_by_tag.loc[time, tag].val
-            self.textBox.setText('cycle: {}\n'.format(time) + val)
-        except KeyError:
-            self.textBox.setText('cycle: {}'.format(time))
+        parent.addItem(self)
+        parent.addItem(self.textBox)
+        print("done trace init")
 
-        self.textBox.setPos(ev.pos())
+    def mouseClickEvent(self, ev):
+        if ev.button() == QtCore.Qt.LeftButton:
+            time = self.settings.get_time(ev.pos().x())
+            tag = self.settings.get_tag(ev.pos().y())
+            try:
+                val = self.idx_by_tag.loc[time, tag].val
+                self.textBox.setText('cycle: {}\n'.format(time) + val)
+            except KeyError:
+                self.textBox.setText('cycle: {}'.format(time))
+
+            self.textBox.setPos(ev.pos())
 
 def plot(df, tags, title):
+    print("start plot")
     tags = list(reversed(tags))
     settings = plot_settings.PlotSettings(tags)
 
     # Qt startup
-    try:
-        app = QtGui.QApplication([])
-    except RuntimeError:
-        app = QtCore.QCoreApplication.instance()
+    app = pg.mkQApp()
 
     win = QtGui.QMainWindow()
-    win.resize(1600,800)
+    win.showMaximized()
     layout = pg.GraphicsLayoutWidget()
     win.setCentralWidget(layout)
     win.setWindowTitle(title)
@@ -60,14 +66,8 @@ def plot(df, tags, title):
     tracePlot.showGrid(x=True, alpha=1)
     tracePlot.setMouseEnabled(x=True, y=False)
 
-    # Click text
-    text = pg.TextItem(color="#ffffff", fill=(20,20,20), anchor=(1,1), border="#ffffff")
-
     # Plot trace
-    s1 = TracePlotItem(df, settings, text)
-
-    tracePlot.addItem(s1)
-    tracePlot.addItem(text)
+    s1 = TracePlotItem(tracePlot, df, settings)
 
     # Scroller setup
     layout.nextRow()
@@ -93,5 +93,6 @@ def plot(df, tags, title):
     tracePlot.sigXRangeChanged.connect(updateRegion)
 
     updateRegion()
+    print("start plot")
 
     return win, app
--- plot_settings.py
+++ plot_settings.py
@@ -7,6 +7,7 @@ class PlotSettings(object):
         colors = '#377eb8,#7b3294,#c2a5cf,#a6dba0,#008837'.split(',')
         self.colors = [mkBrush(col) for col in colors]
         self.tags = tags
+        self.row_pos = dict((tag, i) for (i, tag) in enumerate(tags))
 
     def get_cycle_count(self, time):
         return (time - 0.0) / 10.0
@@ -15,7 +16,7 @@ class PlotSettings(object):
         return int(cycle) * 10
 
     def get_row_pos(self, tag):
-        return self.tags.index(tag)
+        return self.row_pos[tag]
 
     def get_tag(self, row):
         return self.tags[int(row + 0.5)]
